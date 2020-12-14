3f9904178be7c9ae9a17f1a38a3d838b78227a14
luoyk <920581171@qq.com>
2020-12-14 21:30:55 +0800

    支持注册自定义配置

--- osf-spring-boot-autoconfigure/src/main/java/com/luoyk/osf/autoconfigure/common/OsfConfigureRegister.java
+++ osf-spring-boot-autoconfigure/src/main/java/com/luoyk/osf/autoconfigure/common/OsfConfigureRegister.java
@@ -12,10 +12,10 @@ import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.AbstractTypeHierarchyTraversingFilter;
 
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * 手动注册Configure
@@ -24,15 +24,14 @@ import java.util.logging.Logger;
  */
 public class OsfConfigureRegister implements ImportBeanDefinitionRegistrar, EnvironmentAware {
 
+    public static final String CUSTOM = "custom";
+
     private final Logger logger = Logger.getLogger(OsfConfigureRegister.class.getName());
 
     private Environment environment;
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry beanDefinitionRegistry) {
-
-        //默认的实现路径
-        final String basePath = "com.luoyk.osf.achieve";
 
         //类路径扫描器
         ClassPathScanningCandidateComponentProvider scanner =
@@ -49,6 +48,25 @@ public class OsfConfigureRegister implements ImportBeanDefinitionRegistrar, Envi
             }
         });
 
+        final String osfServer = environment.getProperty("osf.server");
+
+        BeanDefinition beanDefinition = CUSTOM.equals(osfServer) ?
+                scannerCustom(annotationMetadata, scanner) :
+                scannerCommon(scanner, osfServer);
+
+        //注册configure
+        beanDefinitionRegistry.registerBeanDefinition("osfConfigure", beanDefinition);
+        logger.info("Initialization osf configure from " + beanDefinition.getBeanClassName());
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public BeanDefinition scannerCommon(ClassPathScanningCandidateComponentProvider scanner, String osfServer) {
+        //默认的实现路径
+        final String basePath = "com.luoyk.osf.achieve";
         //开始搜索默认路径下的所有类
         final Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents(basePath);
 
@@ -57,28 +75,36 @@ public class OsfConfigureRegister implements ImportBeanDefinitionRegistrar, Envi
             throw new RuntimeException("No found osf achieve on the classpath");
         }
 
-        //如果找到的类数量大于1,抛出异常
-        if (candidateComponents.size() > 1) {
-            StringBuilder builder = new StringBuilder("found achieve \n");
-            for (BeanDefinition candidateComponent : candidateComponents) {
-                final String fileSystemName = Optional.ofNullable(candidateComponent.getBeanClassName())
+        final Map<String, BeanDefinition> serverSet = candidateComponents.stream()
+                .collect(Collectors.toMap(bd -> Optional.ofNullable(bd.getBeanClassName())
                         .orElseThrow(() -> new RuntimeException("There can only be one osf achieve on the classpath"))
                         .replace(basePath, "")
                         .replaceFirst("\\.", "")
-                        .split("\\.")[0];
-                builder.append(fileSystemName).append("\n");
-            }
-            builder.append("There can only be one osf achieve on the classpath");
-            throw new RuntimeException(builder.toString());
-        }
+                        .split("\\.")[0], Function.identity()));
 
-        //注册configure
-        beanDefinitionRegistry.registerBeanDefinition("osfConfigure", (BeanDefinition) candidateComponents.toArray()[0]);
-        logger.info("Initialization osf configure from " + ((BeanDefinition) candidateComponents.toArray()[0]).getBeanClassName());
+        final Set<String> serverKeySet = serverSet.keySet();
+        logger.info("Found osf achieve " + Arrays.toString(serverKeySet.toArray()) + " on the classpath");
+
+        return Optional.ofNullable(serverSet.get(osfServer))
+                .orElseThrow(() -> new RuntimeException("No found osf achieve '" +
+                        osfServer + "' ," +
+                        "osf.server must one of " + Arrays.toString(serverKeySet.toArray()) +
+                        " or 'custom'"
+                ));
     }
 
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
+    public BeanDefinition scannerCustom(AnnotationMetadata annotationMetadata, ClassPathScanningCandidateComponentProvider scanner) {
+        String className = annotationMetadata.getClassName();
+        String basePath = className.substring(0, className.lastIndexOf("."));
+        final Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents(basePath);
+        if (candidateComponents.size() == 0) {
+            throw new RuntimeException("No found custom osf achieve");
+        }
+
+        if (candidateComponents.size() > 1) {
+            throw new RuntimeException("There can only be one custom osf achieve");
+        }
+
+        return (BeanDefinition) candidateComponents.toArray()[0];
     }
 }
