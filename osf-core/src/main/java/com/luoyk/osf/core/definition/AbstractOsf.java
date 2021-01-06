b90256af347009acb6955d6c945c789f09f5a8af
luoyk <920581171@qq.com>
2021-01-06 22:15:26 +0800

    提供文件格式的常用操作方法

--- osf-core/src/main/java/com/luoyk/osf/core/definition/AbstractOsf.java
+++ osf-core/src/main/java/com/luoyk/osf/core/definition/AbstractOsf.java
@@ -2,7 +2,14 @@ package com.luoyk.osf.core.definition;
 
 import com.luoyk.osf.core.definition.achieve.FileAction;
 import com.luoyk.osf.core.definition.achieve.PictureAction;
+import com.luoyk.osf.core.exception.OsfException;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.util.MimeTypeUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +19,11 @@ import java.util.UUID;
  * @author luoyk
  */
 public abstract class AbstractOsf {
+
+    /**
+     * MimeType BMP文件
+     */
+    public static final String MIME_TYPE_BMP = "image/bmp";
 
     /**
      * 用于保存默认和自定义的操作流程
@@ -36,6 +48,102 @@ public abstract class AbstractOsf {
      */
     public String getTempId() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    /**
+     * 获得文件后缀名
+     *
+     * @param filename 文件名
+     * @return suffix name
+     */
+    public String getFileSuffixName(String filename) {
+        final int lastDot = filename.lastIndexOf('.');
+        return filename.substring(lastDot + 1);
+    }
+
+    /**
+     * 获得新的文件名
+     *
+     * @param prefix 文件名
+     * @param suffix 后缀名
+     * @return ${prefix}.${suffix}
+     */
+    public String concatFilename(String prefix, String suffix) {
+        return prefix + '.' + suffix;
+    }
+
+    /**
+     * 查找文件的 mimeType
+     *
+     * @param filename 文件名
+     * @return mimetype/content-type
+     */
+    public String getMimeType(String filename) {
+        final String contentType = URLConnection.getFileNameMap().getContentTypeFor(filename);
+        if (contentType == null) {
+            return MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE;
+        }
+        return contentType;
+    }
+
+    /**
+     * 判断文件是否是图片
+     *
+     * @param filename 文件名
+     * @return 是否是图片类型
+     */
+    public boolean isImage(String filename) {
+        switch (getMimeType(filename)) {
+            case MimeTypeUtils.IMAGE_PNG_VALUE:
+            case MimeTypeUtils.IMAGE_GIF_VALUE:
+            case MimeTypeUtils.IMAGE_JPEG_VALUE:
+            case MIME_TYPE_BMP:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * 压缩/转换图片
+     *
+     * @param inputStream 图片文件输入流
+     * @param suffix      后缀名
+     * @return 图片文件字节
+     */
+    public byte[] compressImage(InputStream inputStream, String suffix) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            Thumbnails.of(inputStream)
+                    .scale(1)
+                    .outputFormat(suffix)
+                    .toOutputStream(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new OsfException("Compress image error");
+        }
+    }
+
+    /**
+     * 压缩/转换图片
+     *
+     * @param inputStream 图片文件输入流
+     * @param suffix      后缀名
+     * @param width       图片宽度
+     * @param height      图片高度
+     * @return 图片文件字节
+     */
+    public byte[] resizeImage(InputStream inputStream, String suffix, int width, int height) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            Thumbnails.of(inputStream)
+                    .size(width, height)
+                    .outputFormat(suffix)
+                    .toOutputStream(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new OsfException("Resize image error");
+        }
     }
 
     @SuppressWarnings("unchecked")
